3cb8270fc49796a80ae6240f45cc3ee050f0b253
Jieun <0330jieun@gmail.com>
2023-05-03 17:45:41 +0900

    Git settings

---
+++ mc2_git/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  mc2_git
+//
+//  Created by 박지은 on 2023/05/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mc2_git/mc2_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  mc2_gitApp.swift
+//  mc2_git
+//
+//  Created by 박지은 on 2023/05/03.
+//
+
+import SwiftUI
+
+@main
+struct mc2_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
