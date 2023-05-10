ea0343aa4184c95e494c4960cc60982862eb7b4e
YoungBin Choi <chldudqlsdl@naver.com>
2023-05-10 22:11:37 +0900

    feat: 영화 스케줄 크롤링

--- mc2_git/ContentView.swift
+++ mc2_git/ContentView.swift
@@ -9,6 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     
+    var dateManager = DateManager()
+    var movieScheduleManager = MovieScheduleManager()
+    @State var allDays : Array<String> = []
+    @State var closedDays : Array<String> = []
+    @State var workingDays : Array<String> = []
+    @State var movieScheduleDataForUser: Array<MovieScheduleDataForUser> = []
+    
     var body: some View {
         
         //        CustomAlertView()
@@ -27,7 +34,7 @@ struct ContentView: View {
             }.background(.black)
             
             //Mark: - 날짜 View, 포스터 View
-            MovieDayView2().padding(.bottom, 20)
+            MovieDayView2(movieScheduleDataForUser: $movieScheduleDataForUser, allDays: $workingDays ).padding(.bottom, 20)
             
             //Mark: - 영화관 이름, 주소
             Rectangle()
@@ -58,17 +65,40 @@ struct ContentView: View {
             }.padding(.leading)
         }
         Spacer()
+            .onAppear{
+                dateManager.fetchDate(theaterName: "인디플러스포항")
+                movieScheduleManager.fetchMovieSchedule(theaterName: "인디플러스포항", date: dateManager.allDays[0])
+                allDays = dateManager.allDays
+                closedDays = dateManager.closedDays
+//                workingDays = allDays
+                for i in closedDays {
+                    if let j = allDays.firstIndex(of: i){
+                        allDays.remove(at: j)
+                    }
+                }
+//                print("!!!!!!!!!!")
+//                print(allDays)
+//                print(closedDays)
+//                print(workingDays)
+//                print("!!!!!!!!!!")
+                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+                    movieScheduleDataForUser = movieScheduleManager.movieScheduleDataForUserList
+                    print(movieScheduleDataForUser)
+                }
+            }
+        
     }//Mark: - End VStack
 }
+    
 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
         //        Mark: - 아이폰 status bar 색상 white로 변경
-        var preferredStatusBarStyle: UIStatusBarStyle {
-            return .lightContent
-        }
+//        var preferredStatusBarStyle: UIStatusBarStyle {
+//            return .lightContent
+//        }
     }
 }
 
