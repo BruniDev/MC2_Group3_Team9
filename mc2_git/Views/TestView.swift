71fadcda8874e7095bc4176f4410d435d745b43a
Cha Seung Hoon <papa021326@naver.com>
2023-05-15 00:35:31 +0900

    Add TestView

--- mc2_git/Views/TestView.swift
+++ mc2_git/Views/TestView.swift
@@ -5,87 +5,236 @@
 //  Created by 차차 on 2023/05/12.
 //
 
-import Foundation
+// import Foundation
 import SwiftUI
 
-let questions = [
-    "Q1",
-    "Q2",
-    "Q3",
-    "Q4",
-    "Q5"
-]
-
-let choices = [
-    ["C1","C2"],
-    ["C3","C4"],
-    ["C5","C6"],
-    ["C7","C8"],
-    ["C9","C10"]
-]
-
 let theaters = [
-    "광주극장",
-    "광주독립영화관",
-    "더숲아트시네마",
-    "씨네아트리좀",
-    "아트하우스모모",
-    "안동중앙시네마",
-    "에무시네마",
-    "오오극장",
-    "인디플러스포항",
-    "인디플러스천안",
-    "인천미림극장",
-    "전주디지털독립영화관",
-    "판타스틱큐브",
-    "필름포럼",
-    "헤이리시네마",
-    "씨네큐브광화문",
-    "오르페오한남",
     "라이카시네마",
-    "모퉁이극장",
-    "씨네인디U",
-    "아리랑시네센터",
-    "영화공간주안",
-    "KT&G상상마당시네마",
-    "KU시네마테크"
+    "에무시네마",
+    "인디플러스포항",
+    "KU시네마테크", // Q5 일반 영화관
+    "오오극장",
+    "오르페오한남",
+    "씨네큐브광화문",
+    "판타스틱큐브"// Q5 아이맥스
 ]
 
-struct QuestionView: View {
-    @Binding var num : Int
-    @Binding var answers : [String]
-    
+struct Q1 : View {
+    var body: some View {
+            NavigationView {
+                VStack {
+                            Text("🎬 영화가 땡기는 주말, 당신은? 😎")
+                                .font(.largeTitle)
+                                
+                            NavigationLink(destination: Q2()) {
+                                ZStack {
+                                        Text("🏃‍♂️ 바로 근처 영화관으로 달려간다 🏃‍♂️")
+                                            .fixedSize(horizontal: false, vertical: true)
+                                            .multilineTextAlignment(.center)
+                                            .padding()
+                                            .frame(width: 350, height: 120)
+                                            .foregroundColor(Color.white)
+                                            .background(Rectangle().fill(Color.blue).shadow(radius: 3).cornerRadius(19))
+                                }
+                            }
+                                    
+                                    
+                            NavigationLink(destination: Q2()) {
+                                ZStack {
+                                        Text("🎫 예매 사이트에서 보고 싶은 영화가 있는지 확인한다 🎫")
+                                            .fixedSize(horizontal: false, vertical: true)
+                                            .multilineTextAlignment(.center)
+                                            .padding()
+                                            .frame(width: 350, height: 120)
+                                            .foregroundColor(Color.white)
+                                            .background(Rectangle().fill(Color.blue).shadow(radius: 3).cornerRadius(19))
+                                    }
+                            }
+                        }
+            }
+            .navigationBarBackButtonHidden()
+        }
+}
+
+struct Q2 : View {
+    var body: some View {
+            NavigationView {
+                VStack {
+                            Text("🏖️ 간만에 생긴 자유시간, 당신이라면? 😜")
+                                .font(.largeTitle)
+                                
+                            NavigationLink(destination: Q3()) {
+                                ZStack {
+                                        Text("🏝️ 혼자서도 잘 놀아요, 나만의 시간을 갖는다 ⌛️")
+                                            .fixedSize(horizontal: false, vertical: true)
+                                            .multilineTextAlignment(.center)
+                                            .padding()
+                                            .frame(width: 350, height: 120)
+                                            .foregroundColor(Color.white)
+                                            .background(Rectangle().fill(Color.blue).shadow(radius: 3).cornerRadius(19))
+                                }
+                            }
+                                    
+                                    
+                            NavigationLink(destination: Q3()) {
+                                ZStack {
+                                        Text("😢 혼자는 외로워, 친구들에게 연락한다 📱")
+                                            .fixedSize(horizontal: false, vertical: true)
+                                            .multilineTextAlignment(.center)
+                                            .padding()
+                                            .frame(width: 350, height: 120)
+                                            .foregroundColor(Color.white)
+                                            .background(Rectangle().fill(Color.blue).shadow(radius: 3).cornerRadius(19))
+                                    }
+                            }
+                        }
+            }
+            .navigationBarBackButtonHidden()
+        }
+}
+
+struct Q3 : View {
+    var body: some View {
+            NavigationView {
+                VStack {
+                            Text("🔥 요즘 가장 핫한 영화를 보고 난 뒤, 당신의 행동은? 😙")
+                                .font(.largeTitle)
+                                
+                            NavigationLink(destination: Q4()) {
+                                ZStack {
+                                        Text("📱 SNS에 자랑하기 👍")
+                                            .fixedSize(horizontal: false, vertical: true)
+                                            .multilineTextAlignment(.center)
+                                            .padding()
+                                            .frame(width: 350, height: 120)
+                                            .foregroundColor(Color.white)
+                                            .background(Rectangle().fill(Color.blue).shadow(radius: 3).cornerRadius(19))
+                                }
+                            }
+                                    
+                                    
+                            NavigationLink(destination: Q4()) {
+                                ZStack {
+                                        Text("📸 사진 찍어 혼자 간직하기 ❤️")
+                                            .fixedSize(horizontal: false, vertical: true)
+                                            .multilineTextAlignment(.center)
+                                            .padding()
+                                            .frame(width: 350, height: 120)
+                                            .foregroundColor(Color.white)
+                                            .background(Rectangle().fill(Color.blue).shadow(radius: 3).cornerRadius(19))
+                                    }
+                            }
+                        }
+            }
+            .navigationBarBackButtonHidden()
+        }
+}
+
+struct Q4 : View {
+    var body: some View {
+            NavigationView {
+                VStack {
+                            Text("👨‍🚒 영화 시작 전, 화재 시 대피 요령을 본 당신의 머릿속에는? 🤔")
+                                .font(.largeTitle)
+                                
+                            NavigationLink(destination: Q5()) {
+                                ZStack {
+                                        Text("🚨 영화관에 불이 나면 어떡하지... 🧠")
+                                            .fixedSize(horizontal: false, vertical: true)
+                                            .multilineTextAlignment(.center)
+                                            .padding()
+                                            .frame(width: 350, height: 120)
+                                            .foregroundColor(Color.white)
+                                            .background(Rectangle().fill(Color.blue).shadow(radius: 3).cornerRadius(19))
+                                }
+                            }
+                                    
+                                    
+                            NavigationLink(destination: Q5()) {
+                                ZStack {
+                                        Text("❓ 영화 언제 시작하지? 빨리 시작해라 💤")
+                                            .fixedSize(horizontal: false, vertical: true)
+                                            .multilineTextAlignment(.center)
+                                            .padding()
+                                            .frame(width: 350, height: 120)
+                                            .foregroundColor(Color.white)
+                                            .background(Rectangle().fill(Color.blue).shadow(radius: 3).cornerRadius(19))
+                                    }
+                            }
+                        }
+            }
+            .navigationBarBackButtonHidden()
+        }
+}
+
+struct Q5 : View {
+    @State var answer : Bool = true
+    var body: some View {
+            NavigationView {
+                VStack {
+                            Text("🪐 크리스토퍼 놀란의 신작이 아이맥스로 촬영됐다던데...? 🦇")
+                                .font(.largeTitle)
+                                
+                    NavigationLink(destination: ResultView(answer: $answer)) {
+                                ZStack {
+                                        Text("🎥 당연히 용∙아∙맥이지~ 🐉")
+                                            .fixedSize(horizontal: false, vertical: true)
+                                            .multilineTextAlignment(.center)
+                                            .padding()
+                                            .frame(width: 350, height: 120)
+                                            .foregroundColor(Color.white)
+                                            .background(Rectangle().fill(Color.blue).shadow(radius: 3).cornerRadius(19))
+                                }
+                    }
+                    .simultaneousGesture(TapGesture().onEnded{
+                                answer = true
+                    })
+                                    
+                                    
+                    NavigationLink(destination: ResultView(answer: $answer)) {
+                                ZStack {
+                                        Text("😭 예매하기 빡쎄다던데... 그냥 일반 영화관에서 봐야겠다 🎭")
+                                            .fixedSize(horizontal: false, vertical: true)
+                                            .multilineTextAlignment(.center)
+                                            .padding()
+                                            .frame(width: 350, height: 120)
+                                            .foregroundColor(Color.white)
+                                            .background(Rectangle().fill(Color.blue).shadow(radius: 3).cornerRadius(19))
+                                    }
+                    }
+                    .simultaneousGesture(TapGesture().onEnded{
+                                answer = false
+                    })
+                }
+            }
+            .navigationBarBackButtonHidden()
+        }
+}
+
+struct ResultView: View {
+    @Binding var answer : Bool
+    @State var randomInd : Int = 0
     var body: some View {
         VStack {
-            Text("\(questions[num])")
-            Spacer()
-            Button {
-                answers.insert("\(num + 1)", at : num)
-                // Move View
-            } label : {
-                Text("\(choices[num][0])")
+            Text("당신의 취향은 ... \(theaters[randomInd])")
+                .bold()
+        }
+        .onAppear{
+            if answer == true {
+                randomInd = Int.random(in: 0..<4)
             }
-            Button {
-                answers.insert("\(num + 2)", at : num)
-                // Move View
-            } label : {
-                Text("\(choices[num][1])")
+            else {
+                randomInd = Int.random(in: 4..<8)
             }
         }
     }
 }
 
-//struct ResultView: View {
-//    var body: some View {
-//
-//    }
-//}
-
 struct TestView: View {
     @State var num: Int
-    @State var answers : [String] = []
+    @State var answers : String = ""
     
     var body: some View{
-        QuestionView(num: $num, answers: $answers)
+        Q1()
     }
 }
