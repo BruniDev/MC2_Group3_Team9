c2ba883379c17da2ed7700e727591d8bccb660c0
Junghyeon Kim <hyeon7427@gmail.com>
2023-05-15 09:42:31 +0900

    Fixed Top line in BottomSheet

--- mc2_git/Views/BottomSheetView.swift
+++ mc2_git/Views/BottomSheetView.swift
@@ -7,65 +7,121 @@
 import SwiftUI
 
 struct BottomSheetView: View {
-    @State private var showSheet = true
     @State private var selected = "내 근처 영화관"
     let segments = ["내 근처 영화관", "내 취향 영화관"]
- 
+    @Binding var theaterName : String
+    @Binding var theaters: [Theater]
+    
+    @Environment(\.presentationMode) var presentationMode
     var body: some View {
-      NavigationView {
-            Button("Show Bottom Sheet") {
-                showSheet = true
-            }
-            
-            .buttonStyle(.borderedProminent)
-            .sheet(isPresented: $showSheet) {
-                VStack{
-                    Picker("Choose course", selection: $selected) {
-                        ForEach(segments, id:\.self) { segment in
-                            Text(segment)
-                                .tag(segment)
-                        }
+        ScrollView(.vertical,showsIndicators: false){
+            VStack( ){
+               
+                Text("영화관 탐색하기")
+                    .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
+                
+                Picker("Choose course", selection: $selected) {
+                    ForEach(segments, id:\.self) { segment in
+                        Text(segment)
+                            .tag(segment)
                     }
-                    .pickerStyle(.segmented)
-                    if selected == segments[0] {
-                        HStack{
-                            Rectangle()
-                                .border(.gray)
-                                .cornerRadius(19)
-                                .frame(width: 109, height: 141)
-                            Rectangle()
-                                .border(.gray)
-                                .cornerRadius(19)
-                                .frame(width: 109, height: 141)
-                            Rectangle()
-                                .border(.gray)
-                                .cornerRadius(19)
-                                .frame(width: 109, height: 141)
-                        }
-                    }
-                    else {
-                        Text("영화관 취향 테스트를 통해 \n 취향에 맞는 영화관을 추천해 드릴게요!")
-                        Button(action : {
-                            print("Button Pressed in BSView")
-                            // TestView
+                }
+                .pickerStyle(.segmented)
+                if selected == segments[0] {
+                    
+                    HStack{
+                        Button(action: {
+                            theaterName = theaters[0].name
                         }) {
-                            Text("테스트 시작하기")
+                            ZStack{
+                                Rectangle()
+                                    .frame(width: 109, height: 141)
+                                    .foregroundColor(.white)
+                                    .border(.gray)
+                                    .cornerRadius(19)
+                                
+                                VStack {
+                                    Circle()
+                                        .frame(width: 60, height: 60)
+                                    Text("\(theaters[0].name)")
+                                        .font(.system(size: 15.0))
+                                        .foregroundColor(.black)
+                                    Text("\(theaters[0].handleDistance())")
+                                        .font(.system(size: 15.0))
+                                        .foregroundColor(.gray)
+                                }
+                            }
                         }
-                        .background(Color.purple)
-                        .cornerRadius(40)
-                        .foregroundColor(.white)
+                        
+                        Button(action: {
+                            theaterName = theaters[1].name
+                        }) {
+                            VStack {
+                                Circle()
+                                    .frame(width: 60, height: 60)
+                                Text("\(theaters[1].name)")
+                                    .font(.system(size: 15.0))
+                                    .foregroundColor(.black)
+                                Text("\(theaters[1].handleDistance())")
+                                    .font(.system(size: 15.0))
+                                    .foregroundColor(.gray)
+                            }
+                            .frame(width: 109, height: 141)
+                            .border(.gray)
+                            .cornerRadius(19)
+                        }
+                        
+                        Button(action: {
+                            theaterName = theaters[2].name
+                        }) {
+                            VStack {
+                                Circle()
+                                    .frame(width: 60, height: 60)
+                                Text("\(theaters[2].name)")
+                                    .font(.system(size: 15.0))
+                                    .foregroundColor(.black)
+                                Text("\(theaters[2].handleDistance())")
+                                    .font(.system(size: 15.0))
+                                    .foregroundColor(.gray)
+                            }
+                            .frame(width: 109, height: 141)
+                            .border(.gray)
+                            .cornerRadius(19)
+                            
+                        }
                     }
-                } .presentationDetents([.fraction(0.1),.fraction(0.25)])
+                }
+                else {
+                    Text("영화관 취향 테스트를 통해 \n 취향에 맞는 영화관을 추천해 드릴게요!")
+                    
+                    NavigationLink(destination: TestView()) {
+                        Text("테스트 시작하기")
+                            .font(.system(size: 20.0))
+                            .fixedSize(horizontal: false, vertical: true)
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .frame(width: 182, height: 64)
+                            .foregroundColor(Color.white)
+                            .background(Color(hex:"5856D6").shadow(radius: 3).cornerRadius(32.5))
+                    }
+                    //                        Button(action : {
+                    //
+                    //                        }) {
+                    //                            Text("테스트 시작하기")
+                    //                        }
+                }
             }
-           
-            Spacer()
         }
-    }
+        .presentationDetents([.fraction(0.05),.fraction(0.30)])
+           Spacer()
+           
+           
+   }
 }
 
-struct BottomSheetView_Previews: PreviewProvider {
-    static var previews: some View {
-        BottomSheetView()
-    }
-}
+//struct BottomSheetView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        BottomSheetView()
+//    }
+//}
 
--- mc2_git/Views/MovieDayView2.swift
+++ mc2_git/Views/MovieDayView2.swift
@@ -20,8 +20,7 @@ struct MovieDayView2: View {
     @Binding var isShowingPopup: Bool
     @Binding var theaterName : String
     @Binding var showSheet : Bool
-    @State private var selected = "내 근처 영화관"
-    let segments = ["내 근처 영화관", "내 취향 영화관"]
+
     
     let calendar = Calendar.current
     var dates = getWeek()
@@ -80,106 +79,8 @@ struct MovieDayView2: View {
                     
                 }
             }
-            .sheet(isPresented: $showSheet) {
-                
-                VStack{
-                    Text("영화관 탐색하기")
-                        
-                    Picker("Choose course", selection: $selected) {
-                        ForEach(segments, id:\.self) { segment in
-                            Text(segment)
-                                .tag(segment)
-                        }
-                    }
-                    .pickerStyle(.segmented)
-                    if selected == segments[0] {
-
-                        HStack{
-                            Button(action: {
-                                theaterName = theaters[0].name
-                            }) {
-                                ZStack{
-                                    Rectangle()
-                                        .frame(width: 109, height: 141)
-                                        .foregroundColor(.white)
-                                        .border(.gray)
-                                        .cornerRadius(19)
-                                    
-                                    VStack {
-                                        Circle()
-                                            .frame(width: 60, height: 60)
-                                        Text("\(theaters[0].name)")
-                                            .font(.system(size: 15.0))
-                                            .foregroundColor(.black)
-                                        Text("\(theaters[0].handleDistance())")
-                                            .font(.system(size: 15.0))
-                                            .foregroundColor(.gray)
-                                    }
-                                }
-                            }
-                            
-                            Button(action: {
-                                theaterName = theaters[1].name
-                            }) {
-                                VStack {
-                                   Circle()
-                                        .frame(width: 60, height: 60)
-                                    Text("\(theaters[1].name)")
-                                        .font(.system(size: 15.0))
-                                        .foregroundColor(.black)
-                                    Text("\(theaters[1].handleDistance())")
-                                        .font(.system(size: 15.0))
-                                        .foregroundColor(.gray)
-                                }
-                                .frame(width: 109, height: 141)
-                                .border(.gray)
-                                .cornerRadius(19)
-                            }
-                            
-                            Button(action: {
-                                theaterName = theaters[2].name
-                            }) {
-                                VStack {
-                                   Circle()
-                                        .frame(width: 60, height: 60)
-                                    Text("\(theaters[2].name)")
-                                        .font(.system(size: 15.0))
-                                        .foregroundColor(.black)
-                                    Text("\(theaters[2].handleDistance())")
-                                        .font(.system(size: 15.0))
-                                        .foregroundColor(.gray)
-                                }
-                                .frame(width: 109, height: 141)
-                                .border(.gray)
-                                .cornerRadius(19)
-                                
-                            }
-                        }
-                    }
-                    else {
-                        Text("영화관 취향 테스트를 통해 \n 취향에 맞는 영화관을 추천해 드릴게요!")
-                        
-                            NavigationLink(destination: TestView()) {
-                                Text("테스트 시작하기")
-                                    .font(.system(size: 20.0))
-                                    .fixedSize(horizontal: false, vertical: true)
-                                    .multilineTextAlignment(.center)
-                                    .padding()
-                                    .frame(width: 182, height: 64)
-                                    .foregroundColor(Color.white)
-                                    .background(Color(hex:"5856D6").shadow(radius: 3).cornerRadius(32.5))
-                            }
-//                        Button(action : {
-//
-//                        }) {
-//                            Text("테스트 시작하기")
-//                        }
-                    }
-                } .presentationDetents([.fraction(0.05),.fraction(0.25)])
-                    
-                    
-                    
-            }
+            .sheet(isPresented: $showSheet) {BottomSheetView(theaterName:
+                                                                $theaterName, theaters: $theaters)}
         }
     }
 }
